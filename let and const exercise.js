6a152f7e74ab94b4778785b50209e409f053aa15
AlexRitc <126170399+AlexRitc@users.noreply.github.com>
2023-04-23 19:24:41 -0700

    Add files via upload

---
+++ let and const exercise.js
@@ -0,0 +1,16 @@
+const PI = 3.14;
+PI = 42; // TypeError
+
+
+
+- What is the difference between ***var*** and ***let***?
+    You can reassign and redeclare var but you can not redecalre let. Let also creates block scope.
+
+- What is the difference between ***var*** and ***const***?
+    You can not redeclare or reassign const and it also creates block scope.Var is the complete opposite
+
+- What is the difference between  and ?
+    You can reassign let but cannot with const.
+
+- What is hoisting?
+    Variables lifted to the top of thescope they are delcared in.
