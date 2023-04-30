45a58aa2613f0fc195d8c3cd6824f8e77f7e26e6
AlexRitc <126170399+AlexRitc@users.noreply.github.com>
2023-04-30 07:06:16 -0700

    Add files via upload

---
+++ Fruit Search/script.js
@@ -0,0 +1,39 @@
+const input = document.querySelector('#fruit');
+const suggestions = document.querySelector('.suggestions ul');
+
+const fruit = ['Apple', 'Apricot', 'Avocado 🥑', 'Banana', 'Bilberry', 'Blackberry', 'Blackcurrant', 'Blueberry', 'Boysenberry', 'Currant', 'Cherry', 'Coconut', 'Cranberry', 'Cucumber', 'Custard apple', 'Damson', 'Date', 'Dragonfruit', 'Durian', 'Elderberry', 'Feijoa', 'Fig', 'Gooseberry', 'Grape', 'Raisin', 'Grapefruit', 'Guava', 'Honeyberry', 'Huckleberry', 'Jabuticaba', 'Jackfruit', 'Jambul', 'Juniper berry', 'Kiwifruit', 'Kumquat', 'Lemon', 'Lime', 'Loquat', 'Longan', 'Lychee', 'Mango', 'Mangosteen', 'Marionberry', 'Melon', 'Cantaloupe', 'Honeydew', 'Watermelon', 'Miracle fruit', 'Mulberry', 'Nectarine', 'Nance', 'Olive', 'Orange', 'Clementine', 'Mandarine', 'Tangerine', 'Papaya', 'Passionfruit', 'Peach', 'Pear', 'Persimmon', 'Plantain', 'Plum', 'Pineapple', 'Pomegranate', 'Pomelo', 'Quince', 'Raspberry', 'Salmonberry', 'Rambutan', 'Redcurrant', 'Salak', 'Satsuma', 'Soursop', 'Star fruit', 'Strawberry', 'Tamarillo', 'Tamarind', 'Yuzu'];
+
+function search(str) {
+  let results = [];
+  if (str.length > 0) {
+    results = fruit.filter(f => f.toLowerCase().includes(str.toLowerCase()));
+  }
+  return results;
+}
+
+function searchHandler(e) {
+  const inputVal = e.target.value;
+  const results = search(inputVal);
+  showSuggestions(results, inputVal);
+}
+
+function showSuggestions(results, inputVal) {
+  if (inputVal.length === 0) {
+    suggestions.innerHTML = '';
+    return;
+  }
+
+  const html = results.map(f => `<li>${f}</li>`).join('');
+  suggestions.innerHTML = html;
+  suggestions.classList.add('has-suggestions');
+}
+
+function useSuggestion(e) {
+  const selected = e.target.innerText;
+  input.value = selected;
+  suggestions.innerHTML = '';
+  suggestions.classList.remove('has-suggestions');
+}
+
+input.addEventListener('input', searchHandler);
+suggestions.addEventListener('click', useSuggestion);
